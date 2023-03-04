2e39377930c42232db462cb853ed851bc5af2d5e
gunom <gu3062@naver.com>
2023-03-04 19:50:42 +0900

    [FEAT] 동시성 처리 테스트 작성

---
+++ bankingService/src/test/kotlin/com/example/bankingservice/AccountServoceTest.kt
@@ -0,0 +1,79 @@
+package com.example.bankingservice
+
+import com.example.bankingservice.domain.account.Account
+import com.example.bankingservice.domain.account.AccountRepository
+import com.example.bankingservice.domain.user.User
+import com.example.bankingservice.domain.user.repository.UserRepository
+import com.example.bankingservice.facade.AccountFacade
+import com.example.bankingservice.service.AccountServiceImpl
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.junit.jupiter.MockitoExtension
+import org.redisson.api.RedissonClient
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+import java.util.stream.IntStream
+import kotlin.test.assertEquals
+
+
+@ExtendWith(MockitoExtension::class)
+@SpringBootTest
+class AccountServiceTest {
+
+    @Autowired
+    private lateinit var accountService: AccountServiceImpl
+
+    @Autowired
+    private lateinit var accountRepository: AccountRepository
+
+    @Autowired
+    private lateinit var userRepository: UserRepository
+
+    @Autowired
+    private lateinit var accountFacade: AccountFacade
+
+    val user1 = User(id = 1, email = "test1@test.com", password = "12341234")
+    val user2 = User(id = 2, email = "test2@test.com", password = "12341234")
+
+    @BeforeEach
+    fun setup() {
+        userRepository.save(user1)
+        userRepository.save(user2)
+    }
+    @Test
+    fun testTransferMoneyConcurrently() {
+        val user1 = userRepository.findById(1).get()
+        val user2 = userRepository.findById(2).get()
+        val account1 = Account(user = user1, accountNumber = "1111-1111-1111", asset = 10000)
+        val account2 = Account(user = user2, accountNumber = "1111-1111-1112", asset = 0)
+        accountRepository.save(account1)
+        accountRepository.save(account2)
+
+        val threadCount = 100
+        val executorService = Executors.newFixedThreadPool(threadCount);
+        val countDownLatch = CountDownLatch(threadCount);
+        IntStream.range(0, threadCount).forEach { e: Int ->
+            executorService.submit {
+                try {
+                    accountFacade.transferMoney(1, 1,"1111-1111-1112", 100)
+                } catch (ex: InterruptedException) {
+                    throw RuntimeException(ex)
+                } finally {
+                    countDownLatch.countDown()
+                }
+            }
+        }
+        countDownLatch.await()
+
+        val finalFromAccount = accountRepository.findByAccountNumber(account1.accountNumber)
+        val finalToAccount = accountRepository.findByAccountNumber(account2.accountNumber)
+
+        assertEquals(0L, finalFromAccount!!.asset)
+        assertEquals(10000L, finalToAccount!!.asset)
+    }
+}
---
+++ bankingService/src/test/kotlin/com/example/bankingservice/UserServiceTest.kt
@@ -0,0 +1,68 @@
+package com.example.bankingservice
+
+import com.example.bankingservice.domain.user.User
+import com.example.bankingservice.domain.user.repository.UserRepository
+import com.example.bankingservice.service.UserService
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.transaction.annotation.Transactional
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+@ExtendWith(SpringExtension::class)
+@SpringBootTest
+@ActiveProfiles("test")
+class UserServiceImplTest {
+
+    @Autowired
+    private lateinit var userService: UserService
+
+    @Autowired
+    private lateinit var userRepository: UserRepository
+
+    @Test
+    fun testSaveUser() {
+        // Given
+        val email = "test@example.com"
+        val password = "password"
+
+        // When
+        userRepository.save(User(email = email, password = password))
+
+        // Then
+        val user = userService.findById(1L)
+        assertEquals(email, user.email)
+    }
+
+    @Test
+    fun testSearchUser() {
+        // Given
+        val user1 = User(email = "test1@example.com", password = "password1")
+        val user2 = User(email = "test2@example.com", password = "password2")
+        val user3 = User(email = "another@example.com", password = "password3")
+        user1.friends = mutableSetOf(user2)
+        user2.friends = mutableSetOf(user1)
+        val users = listOf(user1, user2, user3)
+        users.forEach { userService.saveUser(it.email, it.password) }
+
+        // When
+        val result = userService.searchUser(1L, "test")
+
+        // Then
+        assertEquals(2, result.size)
+        assertTrue(result.any { it.email == user1.email })
+        assertTrue(result.any { it.email == user2.email })
+        assertFalse(result.any { it.email == user3.email })
+        assertEquals(user2.id, result.find { it.email == user1.email }!!.id)
+        assertEquals(user1.id, result.find { it.email == user2.email }!!.id)
+        assertTrue(result.all { it.isFriend })
+    }
+}
